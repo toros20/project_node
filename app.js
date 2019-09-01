7d44a633982a7d136307c9969952759556156eff
Marvin Toro <marvinlink07@gmail.com>
2019-08-31 20:41:15 -0600

    mas cambios

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ var app = express();
 app.set("view engine", "jade");
 
 app.get("/",function(req,res){
-    res.render("index", {saludo:"hola mundo Toro"});
+    res.render("index");
 })
 app.listen(8080);

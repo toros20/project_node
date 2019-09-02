2c42966b34cf1687fdfb90fce6091f6e7538f931
Marvin Toro <marvinlink07@gmail.com>
2019-09-01 23:32:06 -0600

    avane de login, bodyparser, layout

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
@@ -7,9 +8,32 @@ var app = express();
     res.send("Hola Mundo");
 }); */
 
+//crearmos un archivo staticos, carpeta public, esset, 
+app.use(express.static('public'));
+//libreria para recibir parametros por formularios, por json y por URL o POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+
+//declaramos el motor de vistas que usaremos
 app.set("view engine", "jade");
 
+//declaramos la ruta raiz y renderizamos la vista llamada indedx 
+//y enviamos el parametro "saludo"  
 app.get("/",function(req,res){
     res.render("index",{saludo: "Bienvenido Toro"});
+});
+
+//declaracion de una ruta tipo GET
+app.get("/login",function(req,res){
+    res.render("login");
 })
+
+//declaracion de una ruta tipo POST
+app.post("/login",function(req,res){
+    console.log("Correo:"+ req.body.email);
+    console.log("Clave:"+req.body.password);
+    res.send("Recibimos tus datos");
+})
+
+//establecemos el puerto 8080 para el servidor
 app.listen(8080);
